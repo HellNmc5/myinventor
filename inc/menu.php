f6f910fbb5a24c4d79f625f2bb3ec4b178b417df
HellNmc5 <kravcenkonikita22@gmail.com>
2025-07-01 18:31:26 +0800

    rewrite and replace some files

---
+++ inc/menu.php
@@ -0,0 +1,12 @@
+<?php
+
+function plugin_pcinfo_getMenuItems() {
+    return [
+        'plugins' => [
+            'title' => 'PC Info',
+            'page'  => '/plugins/pcinfo/front/mypage.php',
+            'options' => []
+        ]
+    ];
+}
+?>
