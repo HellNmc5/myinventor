72f8b6304fa793f31bcdff54f9bcc75a1d3541e3
HellNmc5 <kravcenkonikita22@gmail.com>
2025-07-03 15:29:35 +0800

    re

--- hook.php
+++ hook.php
@@ -1,20 +1,13 @@
 <?php
 
-function plugin_version_myinventor() {
-    return [
-        'name'           => 'My Inventor',
-        'version'        => '1.0.0',
-        'author'         => 'Your Name',
-        'license'        => 'GPLv2+',
-        'homepage'       => 'https://github.com/HellNmc5/myinventor',
-        'minGlpiVersion' => '10.0.0',
-    ];
-}
-
 function plugin_myinventor_install() {
     return true;
 }
 
 function plugin_myinventor_uninstall() {
     return true;
+}
+
+function plugin_myinventor_getAddSearchOptions($itemtype) {
+    return [];
 }
